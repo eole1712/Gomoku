1d2eba39a78ab662489f1511de19d9cd5b159d56
JeanGamain <jean.gamain@epitech.eu>
2015-11-22 19:19:48 +0100

    Regle win

--- srcs/rules/Win.cpp
+++ srcs/rules/Win.cpp
@@ -10,15 +10,45 @@ Win::Win()
 Win::~Win()
 {}
 
-bool	Win::canEatThis(IGameMap *,
-			unsigned int , unsigned int ,
-			unsigned int , unsigned int ,
-			IGameMap::caseContent , IGameMap::caseContent) const
+bool	Win::canEatThis(IGameMap *map,
+			int x, int y,
+			int Tx, int Ty,
+			IGameMap::caseContent v) const
 {
-  /*
-  if (map.isIn(x - vecTX, y - vecTY) &&
-      map.getCase(x - vecTX, y - vecTY) != ) );
-*/
+  const IGameMap::caseContent	ok[4][5] =
+    {
+      { IGameMap::EMPTY, IGameMap::EMPTY, IGameMap::RED, IGameMap::RED, IGameMap::BLUE },
+      { IGameMap::EMPTY, IGameMap::BLUE, IGameMap::RED, IGameMap::RED, IGameMap::EMPTY },
+      { IGameMap::BLUE, IGameMap::RED, IGameMap::RED, IGameMap::EMPTY, IGameMap::EMPTY },
+      { IGameMap::EMPTY, IGameMap::RED, IGameMap::RED, IGameMap::BLUE, IGameMap::EMPTY }
+    };
+  IGameMap::caseContent		cmp[5];
+  bool				swap = v != IGameMap::RED;
+
+  int tmp[2] = { x - (2 * Tx), y - (2 * Ty) };
+  for (int i = 0; i < 5; i++)
+    {
+      if (tmp[0] < 0 || tmp[1] < 0 ||
+	  tmp[0] > 18 || tmp[0] > 18)
+	cmp[i] = IGameMap::BLUE;
+      else if (tmp[0] == x && tmp[1] == y)
+	cmp[i] = IGameMap::RED;
+      else
+	{
+	  cmp[i] = map->getCase(tmp[0], tmp[1]);
+	  if (swap && cmp[i] != IGameMap::EMPTY)
+	    cmp[i] = (cmp[i] == IGameMap::RED) ? IGameMap::BLUE : IGameMap::RED;
+	}
+      tmp[0] += Tx;
+      tmp[1] += Ty;
+    }
+  for (int i = 0; i < 4; i++)
+    {
+      int j = 0;
+      for (; j < 5 && cmp[j] == ok[i][j]; j++);
+      if (j == 5)
+	return true;
+    }
   return false;
 }
 
@@ -26,9 +56,8 @@ bool	Win::isOk(IGame* game)
 {
   IGameMap::caseContent	v =
     (game->getActivePlayer()->getColor() == IPlayer::BLUE) ? GameMap::BLUE : GameMap::RED;
-  GameMap::caseContent	nope = (v == GameMap::BLUE) ? GameMap::RED : GameMap::BLUE;
   IGameMap *	map = game->getMap();
-  char		vecTest[4][2] = {{1, 0}, {0, 1}, {1, 1}, {-1, 1}};
+  const char	vecTest[4][2] = {{1, 0}, {0, 1}, {1, 1}, {-1, 1}};
   int		x = game->getActivePlayer()->getX();
   int		y = game->getActivePlayer()->getY();
   int		pos[2][2];
@@ -52,7 +81,8 @@ bool	Win::isOk(IGame* game)
 	     pos[0][0] < 19 && pos[0][0] >= 0 &&
 	     pos[0][1] < 19 && pos[0][1] >= 0 &&
 	     map->getCase(pos[0][0], pos[0][1]) == v &&
-	     canEatThis(map, pos[0][0], pos[0][1], vecTest[i[0]][0], vecTest[i[0]][1], v, nope))
+	     canEatThis(map, pos[0][0], pos[0][1], vecTest[(i[0] + 1) % 4][0],
+			vecTest[(i[0] + 1) % 4][1], v))
 	{
 	  i[1]++;
 	  pos[0][0] += vecTest[i[0]][0];
@@ -69,7 +99,8 @@ bool	Win::isOk(IGame* game)
 	     pos[1][0] < 19 && pos[1][0] >= 0 &&
 	     pos[1][1] < 19 && pos[1][1] >= 0 &&
 	     map->getCase(pos[1][0], pos[1][1]) == v &&
-	     canEatThis(map, pos[0][0], pos[0][1], vecTest[i[0]][0], vecTest[i[0]][1], v, nope))
+	     canEatThis(map, pos[0][0], pos[0][1], vecTest[(i[0] + 1) % 4][0],
+			vecTest[(i[0] + 1) % 4][1], v))
 	{
 	  i[1]++;
 	  pos[1][0] += vecTest[i[0]][0];
