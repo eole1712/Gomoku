bacc7020a8312e6d60878946fe7fb85a850253fc
jean.gamain <jean.gamain@epitech.eu>
2016-01-20 17:24:27 +0100

    rule DoubleThree ok sauf double alignement sur meme axe

--- srcs/rules/DoubleThree.cpp
+++ srcs/rules/DoubleThree.cpp
@@ -4,8 +4,19 @@
 #include "IPlayer.hpp"
 #include "DoubleThree.hpp"
 
+#include <iostream>
+
 DoubleThree::DoubleThree()
-  : _lastError(std::string("Regle des trois non respecté !"))
+  : _lastError(std::string("Regle des trois non respectée !")),
+    _axis{{1, 0}, {0, 1}, {1, 1}, {-1, 1}}, _boundLimit{-1, 19},
+    _validCase
+    {
+      { IGameMap::EMPTY, IGameMap::RED, IGameMap::RED, IGameMap::RED, IGameMap::EMPTY},
+      { IGameMap::EMPTY, IGameMap::RED, IGameMap::EMPTY, IGameMap::RED, IGameMap::RED},
+      { IGameMap::EMPTY, IGameMap::RED, IGameMap::RED, IGameMap::EMPTY, IGameMap::RED},
+      { IGameMap::RED, IGameMap::RED, IGameMap::EMPTY, IGameMap::RED, IGameMap::EMPTY},
+      { IGameMap::RED, IGameMap::EMPTY, IGameMap::RED, IGameMap::RED, IGameMap::EMPTY}
+    }
 {}
 
 DoubleThree::~DoubleThree()
@@ -21,76 +32,102 @@ std::string const	&DoubleThree::getError() const
   return _lastError;
 }
 
-bool		DoubleThree::isOk(IGame *)
+bool		DoubleThree::isOk(IGame * game)
 {
-  // Case::caseContent	v =
-  //   (game->getActivePlayer()->getColor() == IPlayer::BLUE) ? Case::BLUE : Case::RED;
-  // Case::caseContent	nope = (v == Case::BLUE) ? Case::RED : Case::BLUE;
-  // IGameMap *	map = game->getMap();
-  // const char	vecTest[4][2] = {{1, 0}, {0, 1}, {1, 1}, {-1, 1}};
-  // int		x = game->getActivePlayer()->getX();
-  // int		y = game->getActivePlayer()->getY();
-  // int		pos[2][2];
-  // char		count = 0;
-  // int		i[3];
+  align		tmp[5];
+  _myCell = (game->getActivePlayer()->getColor() == IPlayer::BLUE) ? GameMap::BLUE : GameMap::RED;
+  _enemyCell = (_myCell == GameMap::RED) ? GameMap::BLUE : GameMap::RED;
+  _playingPosition =
+    {
+      static_cast<int>(game->getActivePlayer()->getX()),
+      static_cast<int>(game->getActivePlayer()->getY())
+    };
+  _map = game->getMap();
+  int i = 0;
+  std::cout << "--------------------------------" << std::endl;
+  for (; i < 4 && !(checkFreeAlign(_playingPosition, _axis[i], &tmp[0])
+		    && haveSecondAlign(i, &tmp[0])); i++);
+  return i == 4;
+}
 
-  // i[0] = 0;
-  // for (;i[0] < 4 && count < 2; i[0]++)
-  //   {
-  //     pos[0][0] = x + vecTest[i[0]][0];
-  //     pos[0][1] = y + vecTest[i[0]][1];
-  //     pos[1][0] = x - vecTest[i[0]][0];
-  //     pos[1][1] = y - vecTest[i[0]][1];
-  //     i[1] = 0;
-  //     i[2] = 0;
-  //     while (i[1] < 4 && i[2] < 2 &&
-  // 	     pos[0][0] < 19 && pos[0][0] >= 0 &&
-  // 	     pos[0][1] < 19 && pos[0][1] >= 0 &&
-  // 	     map->getCase(pos[0][0], pos[0][1]) != nope)
-  // 	{
-  // 	  i[2] += (map->getCase(pos[0][0], pos[0][1]) == v);
-  // 	  i[1]++;
-  // 	  pos[0][0] += vecTest[i[0]][0];
-  // 	  pos[0][1] += vecTest[i[0]][1];
-  // 	}
-  //     if (i[2] == 2 &&
-  // 	  pos[0][0] < 19 && pos[0][0] >= 0 &&
-  // 	  pos[0][1] < 19 && pos[0][1] >= 0 &&
-  // 	  map->getCase(pos[0][0], pos[0][1]) == Case::EMPTY &&
-  // 	  pos[1][0] < 19 && pos[1][0] >= 0 &&
-  // 	  pos[1][1] < 19 && pos[1][1] >= 0 &&
-  // 	  map->getCase(pos[1][0], pos[1][1]) == Case::EMPTY)
-  // 	{
-  // 	  count++;
-  // 	  if (count == 2)
-  //             return false;
-  // 	}
-  //     i[1] = 0;
-  //     i[2] = 0;
-  //     while (i[1] < 4 && i[2] < 2 &&
-  // 	      pos[1][0] < 19 && pos[1][0] >= 0 &&
-  // 	      pos[1][1] < 19 && pos[1][1] >= 0 &&
-  // 	     map->getCase(pos[1][0], pos[1][1]) != nope)
-  // 	{
-  // 	  i[2] += (map->getCase(pos[1][0], pos[1][1]) == v);
-  // 	  i[1]++;
-  // 	  pos[1][0] -= vecTest[i[0]][0];
-  // 	  pos[1][1] -= vecTest[i[0]][1];
-  // 	}
-  // 	if (i[2] == 2)
-  // 	  {
-  // 	    int tmp[2] = {x + (2 * vecTest[i[0]][0]), y + (2 * vecTest[i[0]][1])};
-  // 	    if (((pos[0][0] < 19 && pos[0][0] >= 0 &&
-  // 		  pos[0][1] < 19 && pos[0][1] >= 0 &&
-  // 		  map->getCase(pos[0][0], pos[0][1]) == Case::EMPTY) ||
-  // 		 (tmp[0] < 19 && tmp[0] >= 0 &&
-  // 		  tmp[1] < 19 && tmp[1] >= 0 &&
-  // 		  map->getCase(tmp[0], tmp[1]) == Case::EMPTY)) &&
-  // 		pos[1][0] < 19 && pos[1][0] >= 0 &&
-  // 		pos[1][1] < 19 && pos[1][1] >= 0 &&
-  // 		map->getCase(pos[1][0], pos[1][1]) == Case::EMPTY)
-  // 		count++;
-  // 	  }
-  //   }
-  return (true);
+bool		DoubleThree::checkFreeAlign(vec2 const & origin, vec2 axis, align * alignOut)
+{
+  std::pair<GameMap::caseContent, vec2>* alignPtr;
+  std::pair<GameMap::caseContent, vec2>	alignement[13];
+  GameMap::caseContent			cell;
+  vec2		pos;
+  bool		swap = _myCell == IGameMap::BLUE;
+  bool		lastCellEmpty = false;
+  char		emptyCellFind = 0;
+  char		alignDirection = 1;
+
+  alignPtr = &alignement[5];
+  for (int i = 0; i < 13; i++)
+    alignement[i] = { IGameMap::BLUE, 0 };
+  alignement[4] = { IGameMap::RED, origin };
+  for (unsigned char j = 0; j < 2; ++j)
+    {
+      pos = origin + axis;
+      for (unsigned char i = 0; i < 4 && pos.inBound(_boundLimit) &&
+	     (cell =  _map->getCase(pos.x, pos.y)) != _enemyCell &&
+	     !(cell == IGameMap::EMPTY && (lastCellEmpty || emptyCellFind == 2)); ++i)
+	{
+	  alignPtr[0].first = cell;
+	  alignPtr[0].second = pos;
+	  if (alignPtr[0].first == IGameMap::EMPTY)
+	    {
+	      lastCellEmpty = true;
+	      emptyCellFind++;
+	    }
+	  else
+	    {
+	      lastCellEmpty = false;  
+	      if (swap)
+		alignPtr[0].first = IGameMap::RED;
+	    }
+	  alignPtr += alignDirection;
+	  pos += axis;
+	}
+      if (!j)
+	{
+	  lastCellEmpty = false;
+	  if (emptyCellFind == 2)
+	    --emptyCellFind;
+	  axis = -axis;
+	  alignPtr = &alignement[3];
+	  alignDirection = -1;
+	}
+    }
+  alignPtr = &alignement[0];
+  while (alignPtr[0].first == IGameMap::BLUE && alignPtr != &alignement[8])
+    ++alignPtr;
+  if (((&alignement[8]) - alignPtr) < 5) // 2 Empty + 3 prise
+    return false;
+  for (unsigned char i = 0; i < 5; i++)
+    {
+      unsigned char j = 0;
+      for (; j < 5 && alignPtr[j].first == _validCase[i][j]; j++);
+      if (j == 5)
+	{
+	  for (unsigned char k = 0; k < 5; ++k)
+	    alignOut[k] = alignPtr[k];
+	  return true;
+	}
+    }
+  return false;
+}
+
+bool		DoubleThree::haveSecondAlign(int const & originAxis, align * alignPtr)
+{
+  align		tmp[5];
+  for (int j = 0; j < 5; j++)
+    {
+      for (int i = 0; i < 4; i++)
+	{
+	  if (i != originAxis && alignPtr[j].first == IGameMap::RED &&
+	      checkFreeAlign(alignPtr[j].second, _axis[i], &tmp[0]))
+	    return true;
+	}
+    }
+  return false;  
 }
--- srcs/rules/DoubleThree.hpp
+++ srcs/rules/DoubleThree.hpp
@@ -1,13 +1,16 @@
 #ifndef DOUBLETHREE_HPP_
 # define DOUBLETHREE_HPP_
 
-# include <string>
-# include "IRule.hpp"
+#include <string>
+#include "IRule.hpp"
+#include "vec2.hpp"
 
 class IGame;
 
 class DoubleThree : public IRule
 {
+  typedef std::pair<GameMap::caseContent, vec2> align;
+
 public:
   DoubleThree();
   virtual ~DoubleThree();
@@ -18,7 +21,16 @@ public:
   virtual bool			isOk(IGame*);
 
 private:
-  std::string		_lastError;
+  const std::string	_lastError;
+  const vec2		_axis[4];
+  const vec2		_boundLimit;
+  const GameMap::caseContent		_validCase[5][5];
+  GameMap::caseContent	_enemyCell;
+  GameMap::caseContent	_myCell;
+  vec2			_playingPosition;
+  IGameMap *		_map;
+  bool			checkFreeAlign(vec2 const & origin, vec2 axis, align * alignOut);
+  bool			haveSecondAlign(int const & originAxis, align * align);
 };
 
 #endif /* !DOUBLETHREE_HPP_ */
