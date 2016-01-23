a6f94b5bc484c6353a356f196da7d36e951030d4
Grisha Ghukasyan <ghukas_g@epitech.eu>
2016-01-23 20:39:38 +0100

    Map Case

--- srcs/core/Case.cpp
+++ srcs/core/Case.cpp
@@ -1,40 +1,103 @@
 #include "Case.hpp"
 
 Case::Case()
-  : _content(EMPTY)
 {
-  this->_heuristic[0] = 0;
-  this->_heuristic[1] = 0;
+    tab[0] = 0;
+    tab[1] = 0;
+    tab[2] = 0;
+    prop = 0;
+    
+    setEmpty(true);
 }
 
 Case::~Case()
-{}
-
-Case::caseContent	Case::getContent() const
 {
-  return (this->_content);
 }
 
-char	Case::getHeuristic(unsigned int numPlayer) const
+bool    Case::isEmpty() const
 {
-  if (numPlayer > 1)
-    return (0);
-  return (this->_heuristic[numPlayer]);
+    return prop & 0b1000;
 }
 
-void	Case::setContent(Case::caseContent content)
+void    Case::setEmpty(bool value)
 {
-  this->_content = content;
-  if (content != caseContent::EMPTY)
+    prop = (prop & 0b0111) + (value * 0b1000);
+}
+
+bool    Case::getColor() const
+{
+    return prop & 0b0100;
+}
+
+void    Case::setColor(bool value)
+{
+    prop = (prop & 0b1011) + (value * 0b0100);
+}
+
+bool    Case::getPosable(bool color)
+{
+    if (color == false)
+        return prop & 0b0010;
+    else
+        return prop & 0b0001;
+}
+
+void    Case::setPosable(bool color, bool value)
+{
+    if (color == false)
+        prop = (prop & 0b1101) + (value * 0b0010);
+    else
+        prop = (prop & 0b1110) + (value * 0b0001);
+}
+
+// 1111111111 = 2^10 - 1
+// 0000011111 = 2^5 - 1
+// 0000100000 = 2^5
+// 1111011111 = (2^10 - 1) - 2^5
+
+bool    Case::getValue2(Case::dir d, Case::pat2 p, bool color) const
+{
+    uint64_t    pos = (63 - (32 * color) - (p * 8 + d));
+
+    return (tab[0] & (uint64_t)(pow(2, pos))) >> pos;
+}
+
+void    Case::setValue2(Case::dir d, Case::pat2 p, bool color, bool value)
+{
+    uint64_t    pos = (63 - (32 * color) - (p * 8 + d));
+    uint64_t    pow_of_pos = std::pow(2, pos);
+    
+    tab[0] = (tab[0] & (UINT64_MAX - pow_of_pos)) + (value * pow_of_pos);
+}
+
+
+bool    Case::getValue3(Case::dir d, Case::pat3 p) const
+{
+    //return tab[0] & (uint64_t)(std::pow(2, (63 - (p * 8 + d))));
+}
+
+void    Case::setValue3(Case::dir d, Case::pat3 p, bool value)
+{
+    if (p < XYX)
     {
-      this->_heuristic[0] = 0;
-      this->_heuristic[1] = 0;
+        int nb = 32 + (p * 8 + d);
+        if (nb <= 63)
+        {
+            uint64_t    pos = std::pow(2, (63 - nb));
+            
+            tab[0] = std::pow(2, 63) + (tab[0] & (pos - 1)) + (value * pos);
+        }
+        else
+        {
+            uint64_t    pos = std::pow(2, (126 - nb));
+            
+            tab[1] = (tab[1] & (pos - 1)) + (value * pos);
+        }
     }
-}
+    else
+    {
+        uint64_t pos = std::pow(2, (17 - (p - 5)));
 
-void	Case::setHeuristic(unsigned int numPlayer, char heuristic)
-{
-  if (numPlayer > 1)
-    return;
-  this->_heuristic[numPlayer] = heuristic;
+        tab[1] = (tab[1] & (pos - 1)) + (value * pos);
+    }
 }
--- srcs/core/Case.hpp
+++ srcs/core/Case.hpp
@@ -1,23 +1,57 @@
-#ifndef CASE_HPP_
-# define CASE_HPP_
+//
+//  Case.hpp
+//  Gomoku
+//
+//  Created by Grisha Ghukasyan on 23/01/2016.
+//
+//
 
-class Case
-{
+#ifndef Case_hpp
+#define Case_hpp
+
+#include <cmath>
+#include <stdint.h>
+
+class Case {
+    
 public:
-  enum	caseContent {EMPTY = 0, BLUE, RED};
+    enum dir {NW = 0, N, NE, W, E, SW, S, SE};
+    enum pat2 {YX = 0, YOX, YOOX, YOOOX};
+    enum pat3 {YXX = 0, YOXX, YOOXX, YXOX, YXOOX, YOXOX, XYX, XYOX, XYOOX, XOYOX};
+    enum pat4 {YXXX = 0, YOXXX, YXOXX, YXXOX, XYXX, XYOXX, XYXOX};
+    enum pat5 {YXXXX = 0, XYXXX, XXYXX};
 
-  Case();
-  ~Case();
+public:
+    Case();
+    ~Case();
+    
+public:
+    void    setValue2(dir, pat2, bool color, bool);
+    void    setValue3(dir, pat3, bool color, bool);
+    void    setValue4(dir, pat4, bool color, bool);
+    void    setValue5(dir, pat5, bool color, bool);
+    
+public:
+    bool    getValue2(dir, pat2, bool color) const;
+    bool    getValue3(dir, pat3, bool color) const;
+    bool    getValue4(dir, pat4, bool color) const;
+    bool    getValue5(dir, pat5, bool color) const;
+    
+public:
+    void    setColor(bool);
+    bool    getColor() const;
 
-  Case::caseContent	getContent() const;
-  char			getHeuristic(unsigned int numPlayer) const;
-
-  void			setContent(Case::caseContent);
-  void			setHeuristic(unsigned int numPlayer, char heuristic);
+public:
+    void    setEmpty(bool);
+    bool    isEmpty() const;
+    
+public:
+    void    setPosable(bool color, bool value);
+    bool    getPosable(bool color);
 
 private:
-  Case::caseContent	_content;
-  char			_heuristic[2];
+    uint64_t    tab[3];
+    uint8_t     prop;
 };
 
-#endif /* !CASE_HPP_ */
+#endif /* Case_hpp */
