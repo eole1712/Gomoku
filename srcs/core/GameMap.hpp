605e111eca964f610420c77595ba5a1a8823b771
stefan_l <stefan_l@epitech.eu>
2016-01-24 04:07:19 +0100

    heuristic tentative

--- srcs/core/GameMap.hpp
+++ srcs/core/GameMap.hpp
@@ -8,30 +8,34 @@ class GameMap
 : public IGameMap
 {
 public:
-  static const unsigned int	size_x = 19;
-  static const unsigned int	size_y = 19;
-  static const vec2		DIR[8] = {{0, -1},
-					  {-1, -1},
-					  {-1, 0},
-					  {-1, 1},
-					  {0, 1},
-					  {1, 1},
-					  {1, 0},
-					  {1, -1}};
+    static const unsigned int	size_x = 19;
+    static const unsigned int	size_y = 19;
+    static const vec2		DIR[8] = {{0, -1},
+                                      {-1, -1},
+                                      {-1, 0},
+                                      {-1, 1},
+                                      {0, 1},
+                                      {1, 1},
+                                      {1, 0},
+                                      {1, -1}};
+    static const Case::caseContent aiColor = Case::BLUE;
+    static const Case::caseContent noaiColor = Case::RED;
 
 public:
     GameMap();
     virtual ~GameMap();
     GameMap(GameMap &);
-    
+
 private:
     Case	_map[size_x][size_y];
-    
+
 public:
     virtual void		clear();
     virtual Case&		getCase(unsigned int x, unsigned int y);
     virtual void		setCase(unsigned int x, unsigned int y, Case::caseContent);
     virtual static bool		isIn(unsigned int x, unsigned int y) const;
+    int evaluate();
+    void print();
 };
 
 #endif /* !GAMEMAP_HPP_ */
